eb4552e5ace02d21261e7ba5544f533329131cb3
Srisawat <work.srisawat@gmail.com>
2026-01-21 22:36:28 +0700

    Use environment variables for Firebase credentials

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,11 +1,13 @@
 
 import * as admin from "firebase-admin";
 
-import serviceAccount from "../firebase.json";
-
 if (!admin.apps.length) {
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+        credential: admin.credential.cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        } as admin.ServiceAccount),
         // databaseURL: "..." // Firestore doesn't always need this if project_id is correct
     });
 }
